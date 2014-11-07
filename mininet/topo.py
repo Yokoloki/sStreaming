01067d8fa3073bf0254e3f45147fd5769fae815d
loki <loki@sdn.sysu.edu.cn>
2014-11-07 17:22:46 +0800

    add L3 support

--- mininet/topo.py
+++ mininet/topo.py
@@ -27,8 +27,13 @@ def deploy(topo):
 
     info("*** Adding switch\n")
     switches = {}
+    as_map = {}
+    ext_switches = set()
+    as_ip_pool = {}
+    inter_as_ip_pool = {}
     for node_info in topo["nodes"]:
         name = "s%d" % node_info["id"]
+        as_map[node_info["id"]] = node_info["as"]
         if node_info["type"] == "ext":
             hex_mac = "F%011x" % node_info["id"]
         else:
@@ -41,25 +46,48 @@ def deploy(topo):
                         hex_mac[10:12]])
         switch = net.addSwitch(name, dpid=mac)
         if node_info["type"] == "ext":
-            host = net.addHost('h%d' % node_info["id"])
-            net.addLink(switch, host)
+            base = as_ip_pool.get(node_info["as"], 1)
+            as_ip_pool[node_info["as"]] = base + 1
+            host = net.addHost('h%d' % node_info["id"],
+                               ip="172.18.%d.%d/24" % (node_info["as"], base))
+            link = net.addLink(host, switch)
+            ext_switches.add(node_info["id"])
         switches[node_info["id"]] = switch
 
     info("*** Creating links\n")
     for link_info in topo["links"]:
         if "args" not in link_info:
-            net.addLink(switches[link_info["src"]],
+            link = net.addLink(switches[link_info["src"]],
                         switches[link_info["dst"]],
                         cls=TCLink,
                         **topo["defaults"]["link"])
         else:
-            net.addLink(switches[link_info["src"]],
+            link = net.addLink(switches[link_info["src"]],
                         switches[link_info["dst"]],
                         cls=TCLink,
                         **link_info["args"])
+        if (link_info["src"] in ext_switches) \
+                and (link_info["dst"] in ext_switches):
+            as1 = as_map[link_info["src"]]
+            as2 = as_map[link_info["dst"]]
+            as1, as2 = min(as1, as2), max(as1, as2)
+            base = inter_as_ip_pool.get((as1, as2), 0)
+            inter_as_ip_pool[(as1, as2)] = base + 4
+            link.intf1.setIP("10.%d.%d.%d/30" % (as1, as2, base+1))
+            link.intf2.setIP("10.%d.%d.%d/30" % (as1, as2, base+2))
+        elif link_info["src"] in ext_switches:
+            as1 = as_map[link_info["src"]]
+            base = as_ip_pool.get(as1, 1)
+            as_ip_pool[as1] = base + 1
+            link.intf1.setIP("172.18.%d.%d/24" % (as1, base))
+        elif link_info["dst"] in ext_switches:
+            as2 = as_map[link_info["dst"]]
+            base = as_ip_pool.get(as2, 1)
+            as_ip_pool[as2] = base + 1
+            link.intf2.setIP("172.18.%d.%d/24" % (as2, base))
 
     info("*** Starting network\n")
-    net.build()
+    net.start()
     c0.start()
     for k, v in switches.items():
         v.start([c0])
